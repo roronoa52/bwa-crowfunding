f2b52c8f616d0a5b656176bbe5e35b10858536bb
roronoa52 <farhan.yudha2016we@gmail.com>
2024-06-19 16:27:43 +0700

    Add Register User

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "crowfunding/db"
+import (
+	"crowfunding/db"
+	"crowfunding/handler"
+	"crowfunding/user"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
-	db.DBConnection()
+	db := db.DBConnection()
 
+	userRepository := user.NewRepository(db)
+
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+	
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run(":3000")
 }
